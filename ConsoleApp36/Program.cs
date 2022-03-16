7a21ea874cf0c74fe5722640dfca208cd06ab873
unknown <tu7gbzmx6@code.edu.az>
2022-03-16 09:36:41 +0400

    ConsoleApp36

---
+++ ConsoleApp36/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Name user = new Name("Kenan");
+         
+            do
+            {
+                Console.WriteLine("Istifadeci adini daxil edin");
+                string username = Console.ReadLine();
+                if (HasUpper(username) && HasLower(username))
+                {
+                    break;
+                }
+            } while (true);
+            
+            Password user=New Password("Kenan123");
+            do
+            {
+                Console.WriteLine("Parol daxil edin");
+                string username = Console.ReadLine();
+                if (HasUpper(Password) && HasLower(Password) && HasDigit(Password))
+                {
+                    break;
+                }
+            } while (true);
+
+        }
+    }
+}
---
+++ ConsoleApp36/User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp36
+{
+    internal class _UserName
+    {
+        public _UserName(string _UserName)
+        {
+            this._UserName = _UserName;
+        }
+        public string _UserName;
+        public string _Password;
+
+        public string Username
+        {
+            get => this.Username;
+            set
+            {
+                if (Username.Length > 5 && Username.Length < 25)
+                {
+                    this._UserName = value;
+                }
+
+    
+        }   }
+            
+        
+        public string Password
+        {
+            get => this.Password;
+            set
+            {
+                for (int i = 0; i < Password.Length; i++)
+                {
+                    if (Char.IsUpper(Password[i]))
+                    {
+                        this._Password = value;
+                    }
+                    if (Char.IsLower(Password[i]))
+                    {
+                        this._Password = value;
+                    }
+                    if (Char.IsDigit(Password[i]))
+                    {
+                        this._Password = value;
+                    }
+                    if (Password.Length > 7 && Password.Length < 25)
+                    {
+                        this._Password = value;
+                    }
+
+                }
+
+            }
+
+
+        }
+    }
+}
